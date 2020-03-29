3f254415244d8a2abb44c7b2ccc45899ffb1b074
SelvaCanabady P <canabadyweb@gmail.com>
2020-03-29 15:11:43 +0000

    Updates

---
+++ precache-manifest.55aff52cca90563fd4e1f7e1b58b8156.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1e17223719918f6e2466c4c0cbc1da",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "b62040c6bcc060fbb642",
+    "url": "/vidly/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c4663ce3257874773dc",
+    "url": "/vidly/static/js/2.64b84e62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vidly/static/js/2.64b84e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62040c6bcc060fbb642",
+    "url": "/vidly/static/js/main.ccea5fe1.chunk.js"
+  },
+  {
+    "revision": "c26ab3b954bf7e228796",
+    "url": "/vidly/static/js/runtime-main.e3c09976.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vidly/static/media/logo.5d5d9eef.svg"
+  }
+]);
